692bb25cbba08b77f0e7a528ae8d7c134af1524b
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-02 18:26:16 +0100

    added documentation to cli wrapper

--- src/Cli.ts
+++ src/Cli.ts
@@ -3,18 +3,32 @@
 import * as path from 'path';
 import {Styleguide} from './Styleguide';
 
+
+/**
+ * create the static styleguide
+ *
+ * TODO: add commandline feedback for success and error case
+ */
 export function build() {
   new Styleguide()
+  /**
+   * initialize the styleguide with the current working
+   * directory of the app and the root of the upfront tool itself.
+   */
   .initialize(process.cwd(), path.resolve(__dirname, '..'))
+  /** resolve styleguide structure */
   .then(function(styleguide) {
+    console.log('Styleguide.read:' ,'start reading ...');
     return styleguide.read();
   })
+  /** create static styleguide structure */
   .then(function(styleguide) {
+    console.log('Styleguide.read:' ,'finished reading');
+    console.log('Styleguide.write:' ,'start writing ...');
     return styleguide.write();
   })
   .then(function(styleguide) {
-    console.log("\n\nREAD FINISHED:\n");
-    // console.log(styleguide);
+    console.log('Styleguide.write:' ,'finished writing');
   })
   .catch(function(e) {
     console.log(e.stack);
@@ -22,6 +36,12 @@ export function build() {
   });
 };
 
+/**
+ * resolve commandline arguments and run the appropriate command
+ *
+ * TODO: manage commandline arguments :)
+ * TODO: add argument to clean the dist folder in beforehand
+ */
 export function command(args:[string]) {
   build();
 };
