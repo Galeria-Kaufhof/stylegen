ad5c361c66f0c65420b62c0b52cd4425a2289649
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-02 18:27:01 +0100

    simplified promise structure of component class

--- src/component.ts
+++ src/component.ts
@@ -6,81 +6,88 @@ import * as Q from 'q';
 import {IComponentConfig} from './Config';
 import {Partial, View} from './Templating';
 
+/**
+ * Components are the representation of the real asset components,
+ * that are represented by a component.json file inside of an component folder.
+ * A Component has a one-to-one relationship to a Node.
+ */
 export class Component {
   id: string;
   partials: Partial[];
   view: View;
   config: IComponentConfig;
 
+  /**
+   * @param config - the parsed component.json file, enriched with current path and namespace.
+   * @param parent - the parent is used to distuingish components and "sub"-components
+   */
   constructor(config:IComponentConfig, parent?: Component) {
     this.config = config;
+    /** TODO: handle parent resolution and sub component naming, atm. it is useless */
     this.id = `${this.config.namespace}.${path.basename(config.path)}`;
   }
 
   buildPartials():Q.Promise<Component> {
-    var d:Q.Deferred<Component> = Q.defer<Component>();
-
     if(!!this.config.partials) {
+
       var partialPromises:Q.Promise<Partial>[] = this.config.partials.map((partialName:Partial) => {
         var p = path.resolve(this.config.path, partialName);
         return new Partial(p).load();
       });
 
-      Q.all(partialPromises)
+      return Q.all(partialPromises)
       .then((partials:Partial[]) => {
         this.partials = partials;
-        d.resolve(this);
-      })
-      .catch((e) => { d.reject(e); });
+        return this;
+      });
 
     } else if(!this.config.partials) {
       // TODO: try to find  *_partial files in component path (this.config.path)
       this.partials = [];
-      d.resolve(this);
+      return Q(this);
     } else {
       this.partials = [];
       console.warn("Component.buildPartials", "Did not found any partials for Component", this.id);
-      d.resolve(this);
+      return Q(this);
     }
-
-    return d.promise;
   }
 
+  /**
+   * at the moment a Component can have exactly one view,
+   * that is available later as executable js function.
+   *
+   * A view is not reusable directly in other views,
+   * to have a reusable snippet, register a Partial instead.
+   */
   buildView():Q.Promise<Component> {
-    var d:Q.Deferred<Component> = Q.defer<Component>();
-
     if(!!this.config.view) {
       var p = path.resolve(this.config.path, this.config.view);
-      new View(p).load()
+
+      return new View(p).load()
       .then((view) => {
         this.view = view;
-        d.resolve(this)
-      })
-      .catch(e => d.reject(e));
+        return this;
+      });
     } else if(!this.config.view) {
       // TODO: try to find  *_view files in component path (this.config.path)
-      d.resolve(this);
+      return Q(this);
     } else {
       console.warn("Component.buildView", "Did not found a view for Component", this.id);
-      d.resolve(this);
+      return Q(this);
     }
-
-    return d.promise;
   }
 
+
+  /**
+   * building a component means to retrieve the flesh and bones of the component,
+   * that are described in its config
+   *
+   * --> Its Partials, its View, and later all other related stuff.
+   */
   build():Q.Promise<Component> {
-    var d:Q.Deferred<Component> = Q.defer<Component>();
-
-    this.buildPartials()
-    // TODO: finish component building
-    .then(() => {
-      return this.buildView();
-    })
-    .then((component) => {
-      d.resolve(component);
-    })
-    .catch(e => d.reject(e));
-
-    return d.promise;
+    /** resolve the component partials at first */
+    return this.buildPartials()
+    /** after that lets read and build its view */
+    .then(() => this.buildView());
   }
 }
