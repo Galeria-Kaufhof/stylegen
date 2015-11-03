06d044ebf6f0f0b34f8340f1505d891ab1da90fa
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-03 10:21:45 +0100

    refactored/hardened and commented ComponentWriter

--- src/ComponentWriter.ts
+++ src/ComponentWriter.ts
@@ -3,6 +3,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as Q from 'q';
+import * as mkdirp from 'mkdirp';
+
 import {Node} from './Node';
 import {IRenderer} from './Renderer';
 import {Component} from './Component';
@@ -17,9 +19,19 @@ interface IViewComponent {
   compiled?: string;
 }
 
+/**
+ * The ComponentWriter is dedicated on how to write components
+ * to the static file structure of the styleguide.
+ *
+ * So in here it is declared how components are wrapped into
+ * the delivered styleguide components.
+ */
 export class ComponentWriter {
+  /** all nodes of the styleguide */
   nodes: Node[];
+  /** template renderer */
   renderer: IRenderer;
+  /** backreference to the styleguide */
   styleguide: Styleguide;
 
   constructor(renderer: IRenderer, nodes: Node[], styleguide: Styleguide ) {
@@ -74,27 +86,54 @@ export class ComponentWriter {
     return d.promise;
   }
 
+  /**
+   * the most basic writer, that handles the resolution of how to
+   * integrated the rendered component views in the target file structure.
+   */
   write():Q.Promise<ComponentWriter> {
     var d:Q.Deferred<ComponentWriter> = Q.defer<ComponentWriter>();
+
+    var context:{} = {};
+    // TODO: move this clatter to plain component listing method
     try {
+      /** get all all components, registered in the styleguide */
       var components:IViewComponent[] = Object.keys(this.styleguide.components)
       .map(key => this.styleguide.components[key])
-      .filter(c => c.config.namespace !== 'sg')
 
+      /** remove components not element of this styleguide configuration */
+      .filter(c => c.config.namespace === this.styleguide.config.namespace)
+
+      /** build the collected IViewComponents */
       .map(component => this.buildComponent(component))
+
+      /** remove components that had no view */
       .filter(c => c !== null);
+
+      /** set context for rendering the component list */
+      context = { components: components };
+
     } catch(e) {
+      /** if some of the above fails, go to hell!! :) */
       d.reject(e);
     }
-    var context = { components: components };
 
     // TODO: handle/secure this law of demeter disaster :D
     var compListTemplate = this.styleguide.components['sg.layout'].view.template;
 
+    /** shorthand to the styleguide config */
     var config = this.styleguide.config;
 
-    Q.nfcall(fs.mkdir, config.target)
-    .then(() => Q.nfcall(fs.writeFile, path.resolve(config.cwd, config.target, "components.html"),  compListTemplate(context)))
+    /** creating the target folder path (like mkdir -p), if it doesn't exist */
+    Q.nfcall(mkdirp, config.target)
+
+    /** create the plain component list */
+    .then(() => Q.nfcall(
+      fs.writeFile,
+      /** to the target root at the moment */
+      path.resolve(config.cwd, config.target, "components.html"),
+      /** apply the layout template with the components context */
+      compListTemplate(context))
+    )
     .then(() => d.resolve(this))
     .catch(e => d.reject(e));
 
