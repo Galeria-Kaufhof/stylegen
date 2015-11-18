836bed4cf4f1b5414d665d8ad7db7c026ce1f182
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-18 12:20:59 +0100

    USXP-640 self rendering doc implementation

--- src/Doc.ts
+++ src/Doc.ts
@@ -2,5 +2,23 @@
 
 import {CompilableContent} from './CompilableContent';
 import {MarkdownRenderer} from './MarkdownRenderer';
+import {ICompilableContent} from './CompilableContent';
+import {IRenderer} from './Renderer';
 
-export class Doc extends CompilableContent {}
+export class Doc extends CompilableContent {
+  private static engine: IRenderer;
+
+  render():string {
+    return this.engine.render(this.raw);
+  }
+
+  static setEngine(engine: IRenderer) {
+    this.engine = engine;
+  }
+
+  static create(path: string, name: string): ICompilableContent {
+    var doc:Doc = new Doc(path, name);
+    doc.engine = this.engine;
+    return doc;
+  }
+}
