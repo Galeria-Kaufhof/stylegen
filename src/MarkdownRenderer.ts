439d815aba3c4eff3c9943b60aa87f9fd3520ae6
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-18 12:18:28 +0100

    USXP-640 simplified markdown renderer

--- src/MarkdownRenderer.ts
+++ src/MarkdownRenderer.ts
@@ -13,23 +13,14 @@ var md = new Remarkable({ quotes: ''});
  */
 export class MarkdownRenderer implements IRenderer {
   public engine: any;
-  private htmlEngine: IRenderer;
 
   constructor(private options?: IRendererOptions) {
     if (!!options && !!options.htmlEngine) {
-      this.htmlEngine = options.htmlEngine;
-
-      md.use(RemarkableUpfront(this.htmlEngine));
+      md.use(RemarkableUpfront(options.htmlEngine));
     }
   }
 
-  public setEngine<Object>(engine: Object) {
-    this.engine = engine;
-    return this;
-  }
-
-  public render(doc: Doc):Doc {
-    doc.compiled = md.render(doc.raw);
-    return doc;
+  public render(raw: string):string {
+    return md.render(raw);
   }
 }
