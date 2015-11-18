e5e769d1bc06859c625ab40607831c7c0541d612
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-18 12:32:42 +0100

    USXP-640 cleaned up renderer

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,12 +1,9 @@
 "use strict";
-
 /**
  * Interface that new Renderers should implement.
  * As an example implementation look for the HandlebarsRenderer.
  */
 export interface IRenderer {
-  engine: IRenderer;
-  setEngine<T>(engine: T): IRenderer;
   render<T>(object: T): T;
 }
 
@@ -14,10 +11,4 @@ export interface IRenderer {
 export interface IRendererOptions {
   namespace?: string;
   htmlEngine?: IRenderer;
-}
-
-export interface IRenderObject {
-  raw?: string;
-  compiled?: string;
-  type: string;
 }
