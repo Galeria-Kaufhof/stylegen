c8db74e211ad54f93f72a526f68dfeb3b03e989c
Falk Hoppe <falkhoppe81@gmail.com>
2015-11-26 13:46:55 +0100

    USXP-674 added commandline arguments to switch between create and export

--- dist/Cli.js
+++ dist/Cli.js
@@ -2,32 +2,39 @@
 var path = require('path');
 var Styleguide_1 = require('./Styleguide');
 var Logger_1 = require('./Logger');
-function resolveStyleguide() {
+function resolveStyleguide(options) {
     return new Styleguide_1.Styleguide()
         .initialize(process.cwd(), path.resolve(__dirname, '..'))
         .then(function (styleguide) {
-        Logger_1.success('Styleguide.prepare:', 'preparing the styleguide target ...');
-        return styleguide.prepare();
+        if (!!options && !!options.prepare) {
+            Logger_1.success('Styleguide.prepare:', 'preparing the styleguide target ...');
+            return styleguide.prepare();
+        }
+        else {
+            return Promise.resolve(styleguide);
+        }
     })
         .then(function (styleguide) {
         Logger_1.success('Styleguide.read:', 'start reading ...');
         return styleguide.read();
+    })
+        .then(function (styleguide) {
+        Logger_1.success('Styleguide.read:', 'finished reading');
+        return Promise.resolve(styleguide);
     });
 }
 /**
  * create the static styleguide
- *
- * TODO: add commandline feedback for success and error case
  */
 function build() {
-    resolveStyleguide()
+    return resolveStyleguide()
         .then(function (styleguide) {
-        Logger_1.success('Styleguide.read:', 'finished reading');
         Logger_1.success('Styleguide.write:', 'start writing ...');
         return styleguide.write();
     })
         .then(function (styleguide) {
         Logger_1.success('Styleguide.write:', 'finished writing');
+        return styleguide;
     })
         .catch(function (e) {
         Logger_1.error("Cli.build", "failed to build Styleguide", e);
@@ -35,7 +42,19 @@ function build() {
         throw (e);
     });
 }
-exports.build = build;
+;
+/**
+ * create styleguide partials, and maybe other exports
+ */
+function createExport() {
+    return resolveStyleguide({ prepare: false })
+        .catch(function (e) {
+        Logger_1.error("Cli.createExport", "failed to build Styleguide", e);
+        console.log(e.callee, e.stack);
+        throw (e);
+    });
+    /** create static styleguide structure */
+}
 ;
 /**
  * resolve commandline arguments and run the appropriate command
@@ -43,8 +62,15 @@ exports.build = build;
  * TODO: manage commandline arguments :)
  * TODO: add argument to clean the dist folder in beforehand
  */
-function command(args) {
-    build();
+function command(command, args) {
+    switch (command) {
+        case "create":
+            return build();
+        case "export":
+            return createExport();
+        default:
+            return Promise.reject(new Error("Not the command you are searching for"));
+    }
 }
 exports.command = command;
 ;
--- src/Cli.ts
+++ src/Cli.ts
@@ -5,7 +5,11 @@ import {Styleguide} from './Styleguide';
 
 import {success, error} from './Logger';
 
-function resolveStyleguide():Promise<Styleguide> {
+interface IResolveOptions {
+  prepare: boolean;
+}
+
+function resolveStyleguide(options?:IResolveOptions):Promise<Styleguide> {
   return new Styleguide()
   /**
    * initialize the styleguide with the current working
@@ -14,30 +18,36 @@ function resolveStyleguide():Promise<Styleguide> {
   .initialize(process.cwd(), path.resolve(__dirname, '..'))
   /** resolve styleguide structure */
   .then(function(styleguide) {
-    success('Styleguide.prepare:' ,'preparing the styleguide target ...');
-    return styleguide.prepare();
+    if (!!options && !!options.prepare) {
+      success('Styleguide.prepare:' ,'preparing the styleguide target ...');
+      return styleguide.prepare();
+    } else {
+      return Promise.resolve(styleguide);
+    }
   })
   .then(function(styleguide) {
     success('Styleguide.read:' ,'start reading ...');
     return styleguide.read();
+  })
+  .then(function(styleguide) {
+    success('Styleguide.read:' ,'finished reading');
+    return Promise.resolve(styleguide);
   });
 }
 
 /**
  * create the static styleguide
- *
- * TODO: add commandline feedback for success and error case
  */
-export function build() {
-  resolveStyleguide()
+function build():Promise<Styleguide> {
+  return resolveStyleguide()
   /** create static styleguide structure */
   .then(function(styleguide) {
-    success('Styleguide.read:' ,'finished reading');
     success('Styleguide.write:' ,'start writing ...');
     return styleguide.write();
   })
   .then(function(styleguide) {
     success('Styleguide.write:' ,'finished writing');
+    return styleguide;
   })
   .catch(function(e) {
     error("Cli.build", "failed to build Styleguide", e);
@@ -47,11 +57,31 @@ export function build() {
 };
 
 /**
+ * create styleguide partials, and maybe other exports
+ */
+function createExport():Promise<Styleguide> {
+  return resolveStyleguide({ prepare: false })
+  .catch(function(e) {
+    error("Cli.createExport", "failed to build Styleguide", e);
+    console.log(e.callee, e.stack);
+    throw(e);
+  });
+  /** create static styleguide structure */
+};
+
+/**
  * resolve commandline arguments and run the appropriate command
  *
  * TODO: manage commandline arguments :)
  * TODO: add argument to clean the dist folder in beforehand
  */
-export function command(args:[string]) {
-  build();
+export function command(command:string, args:[string]):Promise<any> {
+  switch(command) {
+    case "create":
+      return build();
+    case "export":
+      return createExport();
+    default:
+      return Promise.reject(new Error("Not the command you are searching for"));
+  }
 };
