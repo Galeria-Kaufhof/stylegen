53e2fe9c7cbc91be8d225a8aa1f6592f5eb46543
Falk Hoppe <falkhoppe81@gmail.com>
2015-12-02 09:16:51 +0100

    USXP-680 added test for content creation inclusively to navigation nesting

--- test/integration/content_test.js
+++ test/integration/content_test.js
@@ -7,21 +7,25 @@ var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 var assert = require('chai').assert;
 
+var jsdom = require("jsdom");
+var jquery = fs.readFileSync(path.resolve(__dirname, "../../node_modules/jquery/dist/jquery.js"), "utf-8");
 
 var rewire = require('rewire');
 var Cli = rewire('../../dist/Cli');
 
 describe('Configuration content:', function() {
-  let testResults = "test/integration/results/content_test";
+  var testCWD = "test/integration/fixtures/content_test";
+  var testResults = "test/integration/results/content_test";
 
-  describe('with a styleguide configuration with given pages', function () {
-    afterEach(function() {
-      return fs.remove(path.resolve(testResults));
+  describe('with given pages', function () {
+
+    afterEach(function(done) {
+      fs.remove(path.resolve(testResults), function() {
+        return done();
+      });
     });
 
-
     it('should create html files for each tag page', function () {
-      let testCWD = "test/integration/fixtures/content_test";
       let a = Cli.__get__('build')({ cwd: testCWD })
 
       // file  assertions!
@@ -36,5 +40,49 @@ describe('Configuration content:', function() {
       return assert.isFulfilled(a, "files available");
     });
 
+    it('should create a nested navigation for the content structure', function () {
+      let a = Cli.__get__('build')({ cwd: testCWD })
+
+      // file  assertions!
+      .then(res => {
+        return new Promise(function(resolve, reject) {
+          jsdom.env({
+            file: path.resolve(testResults, 'atoms.html'),
+            src: [jquery],
+            done: function (err, window) {
+              if (!!err) {
+                console.log("ERR", err);
+                reject(err);
+              }
+              var nav = window.$('.test-stylegen-main-nav');
+              var navEntries = window.$('.test-stylegen-main-nav > a');
+              var childLinks = nav.find('.children');
+
+              if (navEntries.length !== 1) {
+                return reject("Expected to have exactly one link in first nav layer");
+              }
+
+              var baseLinkText = 'Atoms';
+              if (navEntries.text().trim() !== baseLinkText) {
+                return reject(`Expected the link to have the link text "${baseLinkText}" instead it has "${navEntries.text()}"`);
+              }
+
+              if (childLinks.length !== 1) {
+                return reject("Expected to have exactly one child link in first nav layer");
+              }
+
+              var childLinkText = 'Forms';
+              if (childLinks.text().trim() !== childLinkText) {
+                return reject(`Expected the link to have the link text "${childLinkText}" instead it has "${childLinks.text().trim()}"`);
+              }
+
+              return resolve(true);
+            }
+          });
+        });
+      });
+
+      return assert.isFulfilled(a, "navigation entries available");
+    });
   });
 });
