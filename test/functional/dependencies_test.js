3c2ce1b3efcdd8f8a86e0b5a5d5e641de4b648b4
Falk Hoppe <falkhoppe81@gmail.com>
2015-12-02 18:25:07 +0100

    USXP-697 added tests for standard dependency behaviour in beforehand

--- test/functional/dependencies_test.js
+++ test/functional/dependencies_test.js
@@ -28,6 +28,41 @@ describe('Configuration dependencies:', function() {
     return done();
   });
 
+  describe('with configured style deps', function () {
+    var sgConfig = YAML.safeLoad(fs.readFileSync(path.resolve(testCWD, 'styleguide.yaml')));
+
+    it('should contain the content inside of the head tag', function () {
+      let sgConfig = YAML.safeLoad(fs.readFileSync(path.resolve(testCWD, 'styleguide.yaml')));
+
+      let a = Cli.__get__('build')({ cwd: testCWD })
+
+      // file  assertions!
+      .then(res => {
+          return fsreadfile(path.resolve(testResults, 'components.html'))
+          .then((content) => {
+            var $ = cheerio.load(content);
+
+            var cssRef = $('head link[rel=stylesheet][href="assets/test.css"]');
+
+            if (cssRef.length !== 1) {
+              return Promise.reject(`Expected to have exactly one reference to the stylesheet, but found ${cssRef.length}`);
+            }
+
+            var jsRef = $('body script[src="assets/test.js"]');
+
+            if (jsRef.length !== 1) {
+              return Promise.reject(`Expected to have exactly one reference to the js dep script tag, but found ${jsRef.length}`);
+            }
+
+            return Promise.resolve(true);
+          });
+
+      });
+
+      return assert.isFulfilled(a, "navigation entries available");
+    });
+  });
+
   describe('with a defined head template', function () {
     var sgConfig = YAML.safeLoad(fs.readFileSync(path.resolve(testCWD, 'styleguide.yaml')));
 
