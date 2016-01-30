c0dcd51efaea3ec2bd382dcbbc1a63d45c6d235f
Falk Hoppe <falkhoppe81@gmail.com>
2016-01-30 08:02:04 +0100

    added test tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var gulp = require("gulp");
 var plumber = require('gulp-plumber');
 
@@ -5,6 +7,9 @@ var plumber = require('gulp-plumber');
 var babel = require("gulp-babel");
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
+
+var mocha = require('gulp-mocha');
+
 
 gulp.task("scripts", function () {
   var tsResult = tsProject.src()
@@ -18,10 +23,22 @@ gulp.task("scripts", function () {
   .pipe(gulp.dest('dist'));
 });
 
+gulp.task('test', function() {
+  process.env.MUTE_CLI_LOG=true
+
+  return gulp.src('test/**/*_test.js', {read: false})
+    // gulp-mocha needs filepaths so you can't have any plugins before it
+    .pipe(mocha());
+});
+
 gulp.task('build', ['scripts']);
 
 gulp.task('default', ['build']);
 
 gulp.task('watch', ['build'] , function() {
   gulp.watch('src/**/*.ts', ['build']);
+});
+
+gulp.task('watch-test', ['test'] , function() {
+  gulp.watch(['dist/**/*', 'test/**/*'], ['test']);
 });
